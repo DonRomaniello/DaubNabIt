e0637ce183e7ff144597c8a2f6b3a0e16569884a
Don Romaniello <don.romaniello@gmail.com>
2022-08-12 14:39:01 -0400

    Initial color state.:

---
+++ src/components/Panels.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+import styles from './Panels.module.css';
+
+export function Panels() {
+
+  return (
+    <div>
+    </div>
+  );
+}
---
+++ src/resources/randomest.js
@@ -0,0 +1,14 @@
+const runAllways = () => {
+  let amount = Math.random()
+
+  if (amount == 1) {
+    console.log(amount)
+  }
+
+
+}
+
+while (0 != 1){
+
+  runAllways()
+}

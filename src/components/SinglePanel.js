b6323287cf663a5007262a312945d1656c5da027
Don Romaniello <don.romaniello@gmail.com>
2022-08-14 10:33:36 -0400

    Add information below panels.

--- src/components/SinglePanel.js
+++ src/components/SinglePanel.js
@@ -6,11 +6,38 @@ export function SinglePanel(props) {
 
   const { color } = props
 
+  const makeRGBObject = (hex) => {
+    const rHex = hex.slice(1,3)
+    const gHex = hex.slice(3,5)
+    const bHex = hex.slice(5)
+
+    const Red = parseInt(rHex, 16)
+    const Green = parseInt(gHex, 16)
+    const Blue = parseInt(bHex, 16)
+
+    return { Red, Green, Blue }
+  }
+
+  const [rgb, setRgb] = useState(makeRGBObject(color))
+
   return (
-    <div className={styles.colorBar}
-      style={{
-        backgroundColor: color,
-      }} />
-    // </div>
-  );
+    <div>
+      <div className={styles.colorBar}
+        style={{
+          backgroundColor: color,
+        }} / >
+      <div className={styles.underHex}>
+      {color.toUpperCase()}
+      </div>
+      <div className={styles.rgbHolder}>
+      {Object.keys(rgb).map((test) => {
+        return (
+        <div className={styles.rgbElement}>
+          {test} : {rgb[test]}
+        </div>
+        )
+      })}
+      </div>
+    </div>
+  )
 }

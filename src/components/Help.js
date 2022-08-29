5d0624306a157754790d066c4c6aa4b587672619
Don Romaniello <don.romaniello@gmail.com>
2022-08-28 20:25:33 -0400

    Initial help toast

--- src/components/Help.js
+++ src/components/Help.js
@@ -12,10 +12,15 @@ import {
       BsQuestionCircleFill
         } from 'react-icons/bs';
 
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import styles from './Help.module.css';
 
 export function Help() {
+
+const notify = (string) => toast("Pasted colors need to be in hexadecimal format (for now). See help for details.");
 
 const gearSize = '55px';
 
@@ -23,11 +28,19 @@ const gearSize = '55px';
 
   return (
     <>
+    <ToastContainer
+      position="top-center"
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+       />
     <div className={`${styles.help} + ' ' + ${paradeOpened && styles.helpParadeOpened}`}>
     <IconContext.Provider value={{ className: styles.context }}>
       <BsQuestionCircleFill
       className={styles.question}
       size={gearSize}
+      onClick={() => notify()}
       />
       </IconContext.Provider>
       </div>
